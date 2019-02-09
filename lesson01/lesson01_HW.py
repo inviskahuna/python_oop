a09aba0e0cdd7d33b51f1cb522aa8fa4beb70f6e
inviskahuna <rvlatyhev@gmail.com>
2019-02-09 11:42:00 +0300

    add split_by_sentences

--- lesson01/lesson01_HW.py
+++ lesson01/lesson01_HW.py
@@ -1,13 +1,19 @@
 import re
-
+import codecs
 
 class ProcessText(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, text):
+        self.text = text
 
-    def split_text(self, text):
-        pass
+    @property
+    def split_by_sentences(self):
+        pattern = re.compile(r'(?<=[.!?…]) ')
+        s = pattern.split(self.text)
+        ret = []
+        for i in s:
+            ret.append(re.sub("|\n|\r", '', i))
+        return ret
 
     def find_freq_word(self, length, lang, text):
         pass
@@ -23,8 +29,12 @@ class ProcessText(object):
 
 
 def main():
-    pass
+    with codecs.open('text.txt', 'r', encoding='utf-8') as text:
+        raw_text = text.read()
 
+    p = ProcessText(raw_text)
+
+    print(p.split_by_sentences)
 
 if __name__ == '__main__':
     main()
