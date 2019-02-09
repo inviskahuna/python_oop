c8d8b564f58a530b913beb2854a6c9c0bb91ad84
inviskahuna <rvlatyhev@gmail.com>
2019-02-09 10:48:00 +0300

    add structure to lesson01 hw

---
+++ lesson01/lesson01_HW.py
@@ -0,0 +1,30 @@
+import re
+
+
+class ProcessText(object):
+
+    def __init__(self):
+        pass
+
+    def split_text(self, text):
+        pass
+
+    def find_freq_word(self, length, lang, text):
+        pass
+
+    def find_links(self, text):
+        pass
+
+    def find_freq_links_domain(self, text):
+        pass
+
+    def decorate_links(self, text, replace):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
