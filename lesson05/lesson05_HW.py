a33a4a95106ad8c66d2d28557b2efdbfa1109110
inviskahuna <rvlatyhev@gmail.com>
2019-02-24 18:52:21 +0300

    yet AnotherSen

--- lesson05/lesson05_HW.py
+++ lesson05/lesson05_HW.py
@@ -1,12 +1,3 @@
-part_of_speech = {
-    "Noun": 0,
-    "Adjective": 1,
-    "Pronoun": 2,
-    "Numeral": 3,
-    "Verb": 4
-}
-
-
 class Word(object):
 
     def __init__(self, value, pos, text=False):
@@ -42,6 +33,37 @@ class Sentence(object):
         return set([x.part_of_speech for x in self.words])
 
 
+class AnotherSen(object):
+    dog = Word("dog", "Noun")  # This is 0
+    eat = Word("eat", "Verb")  # This is 1
+    bone = Word("bone", "Noun")  # This is 2
+    words = [dog, eat, bone]
+    sen = []
+    pos = []
+
+    def __init__(self, word_list):
+        self.word_list = word_list
+        for i in self.word_list:
+            self.sen.append(self.words[i])
+
+    def __str__(self):
+        str_out = []
+        for i in self.sen:
+            str_out.append(i.value)
+        return " ".join(str_out)
+
+    def show(self):
+        self.__str__()
+
+    @property
+    def content(self):
+        return self.word_list
+
+    @property
+    def show_parts(self):
+        return set([x.part_of_speech for x in self.sen])
+
+
 def main():
     dog = Word("dog", "Noun")
     eat = Word("eat", "Verb")
@@ -51,6 +73,11 @@ def main():
     print(s)
     print(s.content)
     print(s.show_parts)
+
+    a = AnotherSen([0, 2])
+    print(a)
+    print(a.content)
+    print(a.show_parts)
 
 
 if __name__ == '__main__':
